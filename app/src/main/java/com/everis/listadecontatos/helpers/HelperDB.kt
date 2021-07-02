a61b41dfae64cd38b285e7f5325bbc8c716af4ef
Guilherme Reis <guilhermereis2009@hotmail.com>
2021-07-02 03:16:18 -0300

    CRUD completo sendo feito conforme ensinado no curso da DIO sobre SQLite
    Signed-off-by: Guilherme Reis <guilhermereis2009@hotmail.com>

---
+++ app/src/main/java/com/everis/listadecontatos/helpers/HelperDB.kt
@@ -0,0 +1,130 @@
+package com.everis.listadecontatos.helpers
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.everis.listadecontatos.feature.listacontatos.model.ContatosVO
+
+class HelperDB(
+        context: Context?
+) : SQLiteOpenHelper(context, NOME_BANCO, null, VERSAO_ATUAL) {
+
+    companion object {
+        private val NOME_BANCO = "contato.db"
+        private val VERSAO_ATUAL = 1
+    }
+
+    val TABLE_NAME = "contato"
+    val COLUMNS_ID = "id"
+    val COLUMNS_NOME = "nome"
+    val COLUMNS_TELEFONE = "telefone"
+    val DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME"
+    val CREATE_TABLE = "CREATE TABLE $TABLE_NAME(" +
+            "$COLUMNS_ID INTEGER NOT NULL," +
+            "$COLUMNS_NOME TEXT NOT NULL," +
+            "$COLUMNS_TELEFONE TEXT NOT NULL," +
+            "" +
+            "PRIMARY KEY(${COLUMNS_ID} AUTOINCREMENT)" +
+            ")"
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(CREATE_TABLE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        if (oldVersion != newVersion) {
+            db?.execSQL(DROP_TABLE)
+            onCreate(db)
+        }
+    }
+
+    fun buscarContatos(busca: String, isBuscaPorId: Boolean = false): List<ContatosVO> {
+        val db = readableDatabase ?: return mutableListOf()
+        var lista = mutableListOf<ContatosVO>()
+        var where: String? = null
+        var args: Array<String> = arrayOf()
+
+        /* outra maneira de fazer o select
+        val sql = "SELECT * FROM $TABLE_NAME WHERE $COLUMNS_NOME LIKE ?"
+        var buscaComPercentual = "%$busca%"
+        var cursor = db.rawQuery(sql, arrayOf(buscaComPercentual))*/
+
+        if (isBuscaPorId) {
+            where = "$COLUMNS_ID = ?"
+            args = arrayOf("$busca")
+        } else {
+            where = "$COLUMNS_NOME LIKE ?"
+            args = arrayOf("%$busca%")
+        }
+
+        var cursor = db.query(TABLE_NAME, null, where, args, null, null, null)
+
+        if (cursor == null) {
+            db.close()
+        }
+
+        while (cursor.moveToNext()) {
+            var contato = ContatosVO(
+                    cursor.getInt(cursor.getColumnIndex(COLUMNS_ID)),
+                    cursor.getString(cursor.getColumnIndex(COLUMNS_NOME)),
+                    cursor.getString(cursor.getColumnIndex(COLUMNS_TELEFONE))
+            )
+            lista.add(contato)
+        }
+        return lista
+    }
+
+    fun salvarContato(contato: ContatosVO) {
+        val db = writableDatabase ?: return
+
+        /* outra maneira de fazer o insert
+        val sql = "INSERT INTO $TABLE_NAME ($COLUMNS_NOME, $COLUMNS_TELEFONE) VALUES (?, ?)"
+        var array = arrayOf(contato.nome, contato.telefone)*/
+
+        var content = ContentValues()
+        content.put(COLUMNS_NOME, contato.nome)
+        content.put(COLUMNS_TELEFONE, contato.telefone)
+        db.insert(TABLE_NAME, null, content)
+
+        /*db.execSQL(sql, array)*/
+
+        db.close()
+    }
+
+    fun deletarContato(id: Int) {
+        val db = writableDatabase ?: return
+        val sql = "DELETE FROM $TABLE_NAME WHERE $COLUMNS_ID = ?"
+        val arg = arrayOf("$id")
+
+        db.execSQL(sql, arg)
+
+        /* outra maneira de fazer o delete
+        val where = "id = ?"
+        val arg = arrayOf("$id")
+        db.delete(TABLE_NAME, where, arg)*/
+
+        db.close()
+    }
+
+    fun updateContato(contato: ContatosVO) {
+        val db = writableDatabase ?: return
+
+        /* outra maneira de fazer o update
+        val content = ContentValues()
+        content.put(COLUMNS_NOME, contato.nome)
+        content.put(COLUMNS_TELEFONE, contato.telefone)
+
+        val where = "id = ?"
+        val arg = arrayOf("${contato.id}")
+
+        db.update(TABLE_NAME, content, where, arg)*/
+
+        val sql = "UPDATE $TABLE_NAME SET $COLUMNS_NOME = ?, $COLUMNS_TELEFONE = ? WHERE $COLUMNS_ID = ?"
+        val args = arrayOf(contato.nome, contato.telefone, contato.id)
+        db.execSQL(sql, args)
+
+        db.close()
+    }
+
+}
